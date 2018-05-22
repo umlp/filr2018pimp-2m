caba2e7c1fcd52c13db53084e937af0cd421d1fb
mferchaud <35867310+mferchaud@users.noreply.github.com>
2018-05-22 17:13:39 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    New Task
+                    Antisèche
                 </div>
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
